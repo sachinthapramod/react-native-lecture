1f1801f87e874fa6f3852a6670b4b0b8bc462182
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2024-12-21 03:56:57 +0000

    add some styles

--- firstapp/app/index.tsx
+++ firstapp/app/index.tsx
@@ -1,21 +1,42 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "teal",
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'teal',
       }}
     >
       <Image
         source={require('../assets/images/react-logo.png')}
         style={styles.logo}
       />
-      <Text>Hello World!</Text>
-      <Text>My First React Native Cross Platform App</Text>
+      <Text style={styles.title}>Hello World!</Text>
+      <Text style={styles.subtitle}>My First React Native Cross Platform App</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'white',
+    marginTop: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: 'white',
+    marginTop: 10,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+
+});
